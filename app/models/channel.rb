7ad6b8966233d582ca60f3ed9c9054a064b542eb
K93wyuta <k93wyuta@gmail.com>
2024-03-28 15:05:55 +0900

    削除機能の実装

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,7 +2,7 @@ class Channel < ApplicationRecord
   has_one_attached :channel_main_image
   has_one_attached :channel_background_image
   has_secure_password
-  has_many :channel_users
+  has_many :channel_users, dependent: :destroy
   has_many :users, through: :channel_users
   has_many :administrators, -> { where(channel_users: { administrator: true }) }, through: :channel_users, source: :user
   has_many :albums
