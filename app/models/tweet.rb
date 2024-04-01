cd73c46e980cc4a98be6a2650f7c5f8e02926011
K93wyuta <k93wyuta@gmail.com>
2024-04-01 18:54:24 +0900

    テストコードの実施

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,14 @@
 class Tweet < ApplicationRecord
   belongs_to :channel_user
   belongs_to :channel
-  has_one_attached :tweet_image 
+  has_one_attached :tweet_image
+
+  validate :either_text_or_image
+
+  private
+    def either_text_or_image
+      unless text.present? || tweet_image.attached?
+        errors.add(:base, "Either text or image must be present.")
+      end
+    end
 end
--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :tweet do
-    
+    tweet_image do
+      Rack::Test::UploadedFile.new(Rails.root.join('public/images/test_profile_image.png'), 'test_profile_image.png')
+    end
+
+    text { Faker::Lorem.paragraph(sentence_count: 2) }
   end
 end
