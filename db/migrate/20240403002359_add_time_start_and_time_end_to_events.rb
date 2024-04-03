e5d3c5e2e047cc60199122b87c2790765a85e7ce
K93wyuta <k93wyuta@gmail.com>
2024-04-03 09:41:54 +0900

    デプロイ調整

--- db/migrate/20240403002359_add_time_start_and_time_end_to_events.rb
+++ db/migrate/20240403002359_add_time_start_and_time_end_to_events.rb
@@ -1,6 +1,5 @@
 class AddTimeStartAndTimeEndToEvents < ActiveRecord::Migration[7.0]
   def change
-    add_column :events, :time_start, :datetime
     add_column :events, :time_end, :datetime
   end
 end
