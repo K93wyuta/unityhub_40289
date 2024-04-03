5ce1c5632820bb68a3d098116b0dd3e2d607f002
K93wyuta <k93wyuta@gmail.com>
2024-04-03 09:56:29 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :channels do
     resources :tweets, only: [:index, :show, :create]
-    resources :events, only: [:index, :new ,:create, :show, :edit ,:update ,:destroy] do
+    resources :topics, only: [:new ,:create,  :edit ,:update ,:destroy] 
+    resources :albums
+    resources :events do
       resources :channel_event_users, only: [:create, :destroy]
     end
-    resources :topics, only: [:new ,:create,  :edit ,:update ,:destroy] 
   end
   resource :sessions, only: [:index, :new, :create, :destroy]
   resources :users, only: [:show, :edit ,:update, :destroy]
