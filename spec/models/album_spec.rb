68991aca6b3645abdaa35b31a098b9570bdac3a0
K93wyuta <k93wyuta@gmail.com>
2024-04-03 19:32:30 +0900

    テストコードの実施

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @album = FactoryBot.build(:album)
+
+    sleep 0.1
+  end
+
+  describe 'アルバム新規投稿' do
+    context 'アルバム新規投稿ができる場合' do
+      it 'すべての項目が存在すれば登録できる' do
+        album = FactoryBot.build(:album)
+        expect(@album).to be_valid
+      end
+    end
+    context 'アルバム新規投稿ができない場合' do
+      it 'titleがないと保存できない' do
+        @album.title = ''
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Title can't be blank")
+      end
+    end
+  end
 end
