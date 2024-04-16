f6e41e4ae9b26a162e14aa7d560ae5316e6a6395
K93wyuta <k93wyuta@gmail.com>
2024-04-16 20:10:20 +0900

    テストコード削除

--- spec/requests/channels_spec.rb
+++ spec/requests/channels_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Channels", type: :request do
-  describe "GET /channels" do
-
-    before do
-      @user = FactoryBot.create(:user)
-      sign_in @user
-    end
-    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
-      get root_path
-      puts "Response location: #{response.location}"
-      expect(response.status).to eq 200
-    end
-  end
 end
